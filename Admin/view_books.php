296ae6d1335de53b6b55a73ed91c18b317e3e5ab
hareeshsprasad <hareeshsprasad@gmail.com>
2021-07-28 15:00:26 +0530

    view_books

--- Admin/view_books.php
+++ Admin/view_books.php
@@ -9,69 +9,36 @@ include "header.php";
                 <div class="card-body">
                   <h4 class="card-title">Book Details</h4>                  
                   <div class="table-responsive pt-3">
-                      <?php
-                      $res=mysqli_query($link,"select * from books");                      
-                      echo'<table class="table table-dark">';
-                      echo'<thead>';
-                      echo'<tr>';
-                    //   echo'<th>';
-                    //   echo  ' No';
-                    //   echo  '</th>';
-                      echo  '<th>';
-                      echo  'Category';
-                      echo  '</th>';
-                      echo  '<th>';
-                      echo  'Book Name';
-                      echo  '</th>';
-                      echo  '<th>';
-                      echo  'Author Name';
-                      echo  '</th>';
-                      echo  '<th>';
-                      echo  'Quantity';
-                      echo  '</th>';
-                      echo  '<th>';
-                      echo  'Available';
-                      echo  '</th>';
-                      echo  '<th>';
-                      echo  'Price';
-                      echo  '</th>';
-                      echo  '<th>';
-                      echo  'Description';
-                      echo  '</th>';
-                      echo  '</tr>';
-                      echo  '</thead>';
-                      while($row=mysqli_fetch_array($res)){
-                      echo  '<tbody>';
-                      echo  '<tr>';
-                    //   echo  '<td>';
-                    //   echo  '1';
-                    //   echo  '</td>';
-                      echo  '<td>';
-                      echo $row['category'];
-                      echo  '</td>';
-                      echo  '<td>';
-                      echo $row['book_name'];  
-                      echo  '</td>';
-                      echo  '<td>';
-                      echo $row['author'];   
-                      echo  '</td>';
-                      echo  '<td>';
-                      echo $row['quantity'];  
-                      echo  '</td>';
-                      echo  '<td>';
-                      echo $row['avaiable_quantity'];   
-                      echo  '</td>';
-                      echo  '<td>';
-                      echo $row['price	'];  
-                      echo  '</td>';
-                      echo  '<td>';
-                      echo $row['description'];  
-                      echo  '</td>';
-                      echo  '</tr>';                     
-                      echo  '</tbody>';
-                      echo  '   </table>';
-                      }
-                    ?>
+                  <?php
+                                $res=mysqli_query($link,"select * from books");
+                                echo"<table class='table table-bordered'>";
+                                echo"<tr>";                               
+                                echo"<th>";echo "Category"; echo"</th>";
+                                echo"<th>";echo "Book Name"; echo"</th>";
+                                echo"<th>";echo "Author"; echo"</th>";
+                                echo"<th>";echo "Quantity"; echo"</th>";
+                                echo"<th>";echo "Available Quantity"; echo"</th>";
+                                echo"<th>";echo "Price"; echo"</th>";
+                                echo"<th>";echo "Description"; echo"<t/h>";                                
+                                echo"<th>";echo "Edit"; echo"</th>";
+                                echo"<th>";echo "Delete"; echo"</th>";
+                                echo"</tr>";
+                                while($row=mysqli_fetch_array($res)){
+                                
+                                echo"<tr>";                                
+                                echo"<td>";echo $row["category"]; echo"</td>";
+                                echo"<td>";echo $row["book_name"]; echo"</td>";
+                                echo"<td>";echo $row["author"];echo"</td>";
+                                echo"<td>";echo $row["quantity"]; echo"</td>";
+                                echo"<td>";echo $row["available_quantity"]; echo"</td>";
+                                echo"<td>";echo $row["price"]; echo"</td>";
+                                echo"<td>";echo $row["description"];echo"</td>";                                
+                                echo"<td>";?><a href="edit_book.php?id=<?php echo $row["id"];?>">Edit</a> <?php echo"</td>";
+                                echo"<td>";?><a href="delete_book.php?id=<?php echo $row["id"];?>">Delete</a> <?php echo"</td>";
+                                echo"</tr>";
+                                 }
+                                 echo"</table>";
+                                ?>
                   </div>
                 </div>
               </div>
