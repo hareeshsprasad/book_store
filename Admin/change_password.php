1711d102d440597528b6c8fee051fe86efad58e5
hareeshsprasad <hareeshsprasad@gmail.com>
2021-08-24 20:34:07 +0530

    change password

--- Admin/change_password.php
+++ Admin/change_password.php
@@ -1,68 +1,35 @@
-<?php 
-        session_start();
-        if(!isset($_SESSION["admin"])){
-            ?>
-            <script type="text/javascript">
-               window.location="login.php";
-           </script>
-            <?php
-               
-           }
-        include "header.php";
-        include "connection.php";
-        ?>
-
-        <!-- page content area main -->
-        <div class="right_col" role="main">
-            <div class="">
-                <div class="page-title">
-                    <div class="title_left">
-                        <h3>Plain Page</h3>
+<?php
+session_start();
+include "connection.php";
+include "header.php";
+?>
+    <div class="col-12 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Change Password</h4>               
+                <form class="forms-sample" action="" method="post">                
+                    <div class="form-group">
+                      <label for="currentpassword">Current Password</label>
+                      <input type="text" class="form-control"  name="currentPassword" placeholder="Current Password">
                     </div>
-
-                    <div class="title_right">
-                        <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
-                            <div class="input-group">
-                                <input type="text" class="form-control" placeholder="Search for...">
-                    <span class="input-group-btn">
-                      <button class="btn btn-default" type="button">Go!</button>
-                    </span>
-                            </div>
-                        </div>
+                    <div class="form-group">
+                      <label for="New Password">New Password</label>
+                      <input type="text" name="newPassword" class="form-control"  placeholder="New Password">
                     </div>
-                </div>
-
-                <div class="clearfix"></div>
-                <div class="row" style="min-height:500px">
-                    <div class="col-md-12 col-sm-12 col-xs-12">
-                        <div class="x_panel">
-                            <div class="x_title">
-                                <h2>Change password</h2>
-
-                                <div class="clearfix"></div>
-                            </div>
-                            <div class="x_content">
-                                <form action="" method="post">
-                                    <div>
-                                        <input type="password" class="form-control" placeholder="Current Password" name="currentPassword" />
-                                    </div>
-                                    <div>
-                                        <input type="password" class="form-control" placeholder="New Password" name="newPassword"   />
-                                    </div>
-                                    <div>
-                                        <input type="password" class="form-control" placeholder="Confirm Password" name="confirmPassword" />
-                                    </div>
-                                    <div class="col-lg-12  col-lg-push-3">
-                                        <input class="btn btn-default submit " type="submit" name="submit1" value="Update Password">
-                                    </div>
-                           </form>
-                           <?php
-                           if(isset($_POST['submit1'])){
-                               $username=$_SESSION["admin"];                            
-                               $currentpass=$_POST['currentPassword'];
-                               $newpass=$_POST['newPassword'];
+                    <div class="form-group">
+                      <label for="qty">Confirm Password</label>
+                      <input type="text" name="confirmPassword" class="form-control" placeholder="Confirm Password">
+                    </div>                    
+                    <input type="submit" name="submit" class="btn btn-primary me-2" value="Submit">
+                    <button class="btn btn-light">Cancel</button>
+                  </form>
+                  <?php
+                           if(isset($_POST['submit'])){
+                               $username=$_SESSION["admin"];                                                       
+                               $currentpass=$_POST['currentPassword'];                           
+                               $newpass=$_POST['newPassword'];                            
                                $confirmpass=$_POST['confirmPassword'];
-                               $checkPassword=mysqli_query($link,"select * from admin where username='$username'");                               
+                               $checkPassword=mysqli_query($link,"select * from admin where email='$username'");                                                   
                                while($row=mysqli_fetch_array($checkPassword)){
                                    $currentPassword=$row['password'];                                   
                                }
@@ -80,19 +47,18 @@
                                </div><?Php
                                }
                                else{
-                                   mysqli_query($link,"update admin set password='$newpass' where username='$username'");
+                                ?>
+                                <div class="alert alert-success col-lg-12 col-lg-push-0">
+                                    <strong style="color:white">Password updated. </strong>
+                               </div><?Php
+                                   mysqli_query($link,"update admin set password='$newpass' where email='$username'");
                                }
                            }
                            
                            ?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+             </div>
         </div>
-        <!-- /page content -->
-        <?php
-        include "footer.php";
-        ?>
-
+    </div>
+    <?php
+    include "footer.php";
+    ?>
